c00b7dbf66fbcd4c70f99da8264a7a2ed2ddd4d1
Vladislav Vinokurov <vinokurov.va@phystech.edu>
2024-08-01 02:10:56 +0300

    Initial commit

---
+++ src/data/dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+
+class ImagesDataset(Dataset):
+    def __init__(self, ):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
---
+++ src/data/df_maker.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def make_dataframe(path):
+    """ """
+    pass
